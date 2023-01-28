6ada784d6e7271f5ee29099951a53d8c2565826e
Fabiano Moreira <fabianomoreira.net@gmail.com>
2023-01-27 21:10:08 -0300

    update foto

---
+++ src/model/constantes.js
@@ -0,0 +1,17 @@
+const constantes = {
+    categorias: [
+        { id: 1, name: "elétrico" },
+        { id: 2, name: "cozinha" },
+    ],
+    tipo_usuario: [
+        { id: 0, name: "admin" },
+        { id: 1, name: "cliente" },
+        { id: 2, name: "" },
+    ],
+    tipo_endereco: [
+        { id: 1, name: "residencia" },
+        { id: 2, name: "comercial" },
+    ]
+}
+
+export default constantes;
