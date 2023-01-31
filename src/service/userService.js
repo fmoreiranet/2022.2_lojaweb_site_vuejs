c6d99b6ee353069424a06ba4cda189e73851df29
Fabiano Moreira <fabianomoreira.net@gmail.com>
2023-01-30 23:44:57 -0300

    update

--- src/service/userService.js
+++ src/service/userService.js
@@ -28,46 +28,6 @@ export default {
         };
         return http.post("/usuario/upload", dataSend, request_config);
     },
-    // uploadFirebase: function (dataSend) {
-    //     let auth = FirebaseApp.auth();
-    //     let storageRef = FirebaseApp.storage().ref();
-
-    //     // Push to child path.
-    //     storageRef.child('images/user' + file.name)
-    //         .put(file, metadata)
-    //         .then(function (snapshot) {
-    //             console.log('Uploaded', snapshot.totalBytes, 'bytes.');
-    //             console.log('File metadata:', snapshot.metadata);
-    //             // Let's get a download URL for the file.
-    //             snapshot.ref.getDownloadURL().then(function (url) {
-    //                 console.log('File available at', url);
-    //                 // document.getElementById('linkbox').innerHTML = '<a href="' + url + '">Click For File</a>';
-    //                 return url
-    //             });
-    //         }).catch(function (error) {
-    //             console.error('Upload failed:', error);
-    //         });
-    // },
-
-    uploadBase64: function (files, idUser) {
-        let base64files = [];
-        for (let f of files) {
-            this.getBase64(f)
-                .then(function (base64) {
-                    base64files.push(base64);
-                })
-        }
-
-        const request_config = {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            }
-        };
-
-        let dataSend = { idUser: idUser, files: base64files };
-        return http.post("/usuario/update/", dataSend, request_config);
-    },
-
     getBase64: function (file) {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
--- src/views/UserPerfil.vue
+++ src/views/UserPerfil.vue
@@ -36,16 +36,6 @@ export default {
                     console.error(err);
                 });
         },
-        upload64() {
-            let files = this.$refs.image.files;
-            userService.uploadBase64(files, this.usuario._id)
-                .then(res => {
-                    console.log(res);
-                    this.perfil = this.updatePhoto(res.data);
-                }).catch(err => {
-                    console.error(err);
-                });
-        },
         updatePhoto(foto) {
             let local = import.meta.env.VITE_APIURL;
             let folder = import.meta.env.VITE_UPLOADURL + "/users/";
@@ -122,7 +112,7 @@ export default {
     <section class="container">
         <div class="input-group mb-3">
             <input type="file" class="form-control" id="inputGroupPhoto" name="file" ref="image" multiple
-                @change="upload64()">
+                @change="upload()">
             <label class="input-group-text" for="inputGroupPhoto"></label>
         </div>
     </section>
